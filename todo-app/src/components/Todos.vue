af85907e0fef84db87da3380a8052c6473d98043
PRanpise <poojapanhale26@gmail.com>
2020-07-17 10:32:25 +0530

    all vue js projects

---
+++ todo-app/src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='ui centered card'>
+    <!-- <div :key="todo.id"  -->
+     <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" />
+    </div> 
+  </div>
+</template>
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"],
+
+  methods: {
+   
+  },
+};
+</script>
